ac288f15e4a3731068fe3141b025cc03618b2c9b
Megha Sharma <83399480+meghasharma2707@users.noreply.github.com>
2022-02-11 21:06:23 +0530

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine, RiStarLine, RiStarSFill } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo, favTodo }) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  });
+
+  const submitUpdate = value => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ''
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        {todo.isFav ? (
+            <>
+              <RiStarSFill
+                  onClick={() => favTodo(todo.id)}
+                  className='fav-icon'
+              />
+            </>
+        ) : (
+            <>
+              <RiStarLine
+                  onClick={() => favTodo(todo.id)}
+                  className='fav-icon'
+              />
+            </>
+        )}
+        <RiCloseCircleLine
+          onClick={() => removeTodo(todo.id)}
+          className='delete-icon'
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className='edit-icon'
+        />
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
